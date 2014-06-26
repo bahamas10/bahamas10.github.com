3d2df6496eaef6dbdbba5dfb7ab24d5112953ce5
Dave Eddy <dave@daveeddy.com>
2014-06-26 19:34:44 -0400

    clean up... still need more

--- static/js/github.js
+++ static/js/github.js
@@ -8,64 +8,64 @@
  * License: MIT
  */
 
-// Call with a user
+// call with a user
 function github(username) {
-  (function(d,t) {
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.src = 'https://api.github.com/users/' + username + '/repos?callback=__my_data&per_page=100';
-    s.parentNode.insertBefore(g,s);
-  }(document, 'script'));
+  var g = document.createElement('script');
+  var s = document.getElementsByTagName('head')[0];
+  g.src = 'https://api.github.com/users/' + username + '/repos?callback=__my_data&per_page=100';
+  s.parentNode.insertBefore(g,s);
 }
 
-// Callback for github
+// callback for github
 function __my_data(data) {
-  (function($) {
-    data = data.data;
-    var columns = 3;
+  data = data.data;
+  var columns = 3;
 
-    // Hardcoded goodness
-    var $elem = $('#github-data'),
-        d = {
-          'repos': [],
-          'forks': []
-        },
-        s = '';
+  // Hardcoded goodness
+  var $elem = $('#github-data');
+  var d = {
+    repos: [],
+    forks: []
+  };
+  var s = '';
 
-    // Separate my forks from repos
-    for (var i in data) {
-      if (data[i].fork) d.forks.push(data[i]);
-      else d.repos.push(data[i]);
+  // Separate my forks from repos
+  for (var i in data) {
+    if (data[i].fork)
+      d.forks.push(data[i]);
+    else
+      d.repos.push(data[i]);
+  }
+
+  // Make the list
+  for (var type in d) {
+    // Make the title
+    s += '<h1>';
+    s += (type === 'repos')
+       ? 'My Projects on GitHub (' + d[type].length + ')'
+       : 'Forked Projects (' + d[type].length + ')';
+    s += '</h1><br />';
+
+    // List the projects
+    for (var i in d[type]) {
+      s += '<div class="github-project">';
+      s += '<h3><a href="' + d[type][i].html_url + '">' + d[type][i].name + '</a></h3>';
+      s += '<blockquote>' + d[type][i].description + '</blockquote>';
+      var watchers = d[type][i].watchers;
+      watchers = watchers === 1 ? (watchers + ' watcher') : (watchers + ' watchers');
+      var forks = d[type][i].forks;
+      forks = forks === 1 ? (forks + ' fork') : (forks + ' forks');
+      s += watchers + ' / ' + forks + '<br />';
+      if (d[type][i].language)
+        s += 'written in ' + d[type][i].language + '<br />';
+      var dobj = new Date(d[type][i].created_at);
+      s += 'created on ' + (dobj.getMonth()+1) + '/' + dobj.getDate() + '/' + dobj.getFullYear();
+      s += '</div>';
+      if (i%columns === columns - 1)
+        s += '<hr class="divider">';
     }
+  }
 
-    // Make the list
-    for (var type in d) {
-      // Make the title
-      s += '<h1>';
-      s += (type === 'repos')
-         ? 'My Projects on GitHub (' + d[type].length + ')'
-         : 'Forked Projects (' + d[type].length + ')';
-      s += '</h1><br />';
+  $elem.html(s);
 
-      // List the projects
-      for (var i in d[type]) {
-        s += '<div class="github-project">';
-        s += '<h3><a href="' + d[type][i].html_url + '">' + d[type][i].name + '</a></h3>';
-        s += '<blockquote>' + d[type][i].description + '</blockquote>';
-        var watchers = d[type][i].watchers;
-        watchers = (watchers === 1) ? watchers + ' watcher' : watchers + ' watchers';
-        var forks = d[type][i].forks;
-        forks = (forks === 1) ? forks + ' fork' : forks + ' forks';
-        s += watchers + ' / ' + forks + '<br />';
-        if (d[type][i].language) s += 'written in ' + d[type][i].language + '<br />';
-        var dobj = new Date(d[type][i].created_at);
-        s += 'created on ' + (dobj.getMonth()+1) + '/' + dobj.getDate() + '/' + dobj.getFullYear();
-        s += '</div>';
-        if (i%columns == columns - 1) s += '<hr class="divider">';
-      }
-    }
-
-    $elem.html(s);
-
-  }(jQuery));
 }
